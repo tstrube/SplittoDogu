de7a7218e82a1507df329175e72136d25b670083
Timo <76815855+tstrube@users.noreply.github.com>
2022-12-13 13:26:38 +0100

    Init

---
+++ Firmware/rules.mk
@@ -0,0 +1,10 @@
+# This file intentionally left blank
+
+ENCODER_ENABLE = yes
+KEY_LOCK_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+SPLIT_KEYBOARD = yes
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
