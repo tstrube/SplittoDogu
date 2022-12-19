6631d651f2e3dc5cdbf7f70e5e7f4c2006dc1b6f
Timo <github.com@timostrube.de>
2022-12-19 12:48:05 +0100

    removed WIN key from Game layout

--- Firmware/keymaps/default/keymap.c
+++ Firmware/keymaps/default/keymap.c
@@ -57,13 +57,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TSW_CUT,KC_TAB ,KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_EQL ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_SLSH,KC_DEL ,
         TSW_CPY,KC_LSFT,KC_LSFT,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_BSLS,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_QUOT,KC_ENT ,KC_BSPC,
         TSW_PST,KC_LCTL,KC_LCTL,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_HOME,KC_END ,KC_B   ,KC_N   ,KC_M   ,TS_COMM,TS_DOT ,KC_RSFT,KC_PSCR,
-        TSW_LCK,KC_CAPS,KC_APP ,TO(_MC),MO(_FN),KC_LGUI,KC_LALT,KC_SPC ,KC_BSPC,MO(_FN),TG(_FN),KC_LOCK,TO(_DF),KC_MPLY,KC_MNXT,KC_MUTE
+        TSW_LCK,KC_CAPS,KC_APP ,TO(_MC),MO(_FN),KC_LALT,KC_LALT,KC_SPC ,KC_BSPC,MO(_FN),TG(_FN),KC_LOCK,TO(_DF),KC_MPLY,KC_MNXT,KC_MUTE
     ),
     [_FN] = LAYOUT(
         QK_BOOT,_______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,_______,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,QK_BOOT,
         KC_NUM ,_______,_______,_______,KC_UP  ,_______,_______,_______,_______,KC_COMM,KC_P7  ,KC_P8  ,KC_P9  ,KC_PMNS,KC_PSLS,KC_NUM ,
-        _______,_______,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,_______,KC_PDOT,KC_P4  ,KC_P5  ,KC_P6  ,KC_PPLS,KC_PAST,_______,
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P0  ,KC_P1  ,KC_P2  ,KC_P3  ,KC_PENT,_______,_______,
+        _______,_______,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,_______,KC_PDOT,KC_P4  ,KC_P5  ,KC_P6  ,KC_PENT,KC_PAST,_______,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_P0  ,KC_P1  ,KC_P2  ,KC_P3  ,KC_PPLS,_______,_______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
     )
 };
